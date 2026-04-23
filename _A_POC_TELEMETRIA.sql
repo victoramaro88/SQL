44546577e1d1c2a01ba108011c7774efb709c589
Victor Amaro <amaro.victor@hotmail.com>
2026-04-23 14:26:28 -0300

    no message

---
+++ _A_POC_TELEMETRIA.sql
@@ -0,0 +1,9 @@
+SELECT TOP(100)
+	POCIdf, AparelhoId, latitude, longitude, DtAfericao, DtInsert
+FROM DB_TELEMETRIA.SchPOC.POC
+ORDER BY POCIdf DESC;
+
+
+SELECT TOP(100)
+	POCIdf, AparelhoId, latitude, longitude, DtAfericao, DtInsert
+FROM DB_TELEMETRIA.SchPOC.POC;
